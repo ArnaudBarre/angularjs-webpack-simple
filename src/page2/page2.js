571233035cc70555e48eef74a7d2376d7277e3a8
Arnaud Barré <arnaudbarremn@gmail.com>
2018-03-14 00:58:23 +0100

    typos

--- src/page2/page2.js
+++ src/page2/page2.js
@@ -3,13 +3,13 @@ import template from './page2.html';
 /* @ngInject */
 function controller($scope, $rootScope, service) {
   $scope.changeHeader = () => {
-    $rootScope.headerText = 'Angularjs is awesome !';
+    $rootScope.headerText = 'AngularJS is awesome !';
   };
 
   service.getTitle().then((title) => {
     $scope.title = title;
   }).catch(() => {
-    $scope.title = 'An error occured';
+    $scope.title = 'An error occurred';
   });
 }
 
